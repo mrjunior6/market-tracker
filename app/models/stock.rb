d5fac2d57b222e8a1bcf9c507086ab0389e35e51
Helio Junior <helio.jr@icloud.com>
2017-10-17 13:57:40 -0200

    fix bugs with JS

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,14 +8,26 @@ class Stock < ApplicationRecord
 		looked_up_stock = Bittrex::Quote.current(ticker_symbol)
 		return nil unless looked_up_stock.market
 
-		new_stock = new(ticker: looked_up_stock.market, name: looked_up_stock.market)
+		coinName = findname(ticker_symbol)
+		new_stock = new(ticker: looked_up_stock.market.upcase, name: coinName)
+		#new_stock.name = findname(ticker_symbol)
 		new_stock.last_price = new_stock.price
 		new_stock
 	end
 
 	def price
 		last_price = Bittrex::Quote.current(ticker).last
+		last_price = ("%f" % last_price).to_f
+		#last_price = helper.number_with_delimiter(last_price, :deliter => ",", :separator => ".")
 		return last_price
+	end
+
+	def self.findname(ticker)
+		str = ""
+		coin = ticker.split('-')[1]		
+		var = Bittrex::Currency.all
+		var.collect.each_with_index {|key,index| (str = key.name ) if key.abbreviation == coin.upcase }
+		return str
 	end
 
 
